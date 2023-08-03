388ec0d365f3f0e913aed446fe11c302d4edadd1
Nagendraindus <130830080+Nagendraindus@users.noreply.github.com>
2023-08-03 17:16:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ const App = () =>{
     return(
         <div className="app">
             <h1>
-                Kutta
+                Chitra Info
             </h1>
             <div className="search">
                 <input placeholder="search here" value={searchTerm}
